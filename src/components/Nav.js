fa042d1657ebf0b4532253ea5ffbb8e28fa1d2f0
PINKDDDD <alfredyuan0317@gmail.com>
2024-01-16 14:56:37 -0800

    mobile responsive change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import {faHouse} from'@fortawesome/free-solid-svg-icons'
 export default function Nav(Props){
     return(
         <nav id='nav'>
-        <button className="mode-btn" onClick={Props.toggleLightMode}>{Props.darkMode ? 'Dark Mode': "Light Mode"}</button>
+        <button className="mode-btn" onClick={Props.toggleLightMode}>{Props.darkMode ? 'Light Mode': "Dark Mode"}</button>
         <ul className = "nav-right">
             <li className = 'li_padding'><a href = '#about'> About</a></li>
             <li className = 'li_padding'><a href = '#skills'> Skills</a></li>
